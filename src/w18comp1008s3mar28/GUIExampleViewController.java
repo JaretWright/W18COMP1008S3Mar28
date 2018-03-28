176afeb26f466bb43ccb69451ce8cb3b612bfec1
JaretWright <jaret.wright@georgiancollege.ca>
2018-03-28 13:25:04 -0400

    Added CheckBox fields

--- src/w18comp1008s3mar28/GUIExampleViewController.java
+++ src/w18comp1008s3mar28/GUIExampleViewController.java
@@ -2,7 +2,10 @@ package w18comp1008s3mar28;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
 
 /**
  * FXML Controller class
@@ -11,6 +14,11 @@ import javafx.fxml.Initializable;
  */
 public class GUIExampleViewController implements Initializable
 {
+    @FXML    private CheckBox homeworkCheckBox;
+    @FXML    private CheckBox sleepCheckBox;
+    @FXML    private CheckBox videoGameCheckBox;
+    @FXML    private CheckBox netflixCheckBox;
+    @FXML    private TextArea weekendTextArea;
 
     /**
      * Initializes the controller class - this method runs once when the
@@ -20,6 +28,29 @@ public class GUIExampleViewController implements Initializable
     public void initialize(URL url, ResourceBundle rb)
     {
         // TODO
-    }    
+    }   
+    
+    /**
+     * This method will update the TextArea with the CheckBoxes
+     * that are selected
+     */
+    public void updateWeekendTextArea()
+    {
+        String output = "This weekend I will....\n";
+        
+        if (this.homeworkCheckBox.isSelected())
+            output += "->homework \n";
+        
+        if (this.sleepCheckBox.isSelected())
+            output += "->sleep...so much\n";
+        
+        if (this.videoGameCheckBox.isSelected())
+            output += "->play video games\n";
+        
+        if (this.netflixCheckBox.isSelected())
+            output += "->watch Netflix";
+        
+        this.weekendTextArea.setText(output);
+    }
     
 }
